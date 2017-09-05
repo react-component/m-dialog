82df3a1a9385598a7cf2eea603098bc2d9ceb896
silentcloud <rjmuqiang@gmail.com>
2017-09-05 17:44:14 +0800

    feat: dialog for mobile

---
+++ examples/react-native/demo.native.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import Dialog from '../../src/Dialog.native';
+
+const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    borderColor: 'red',
+    padding: 5,
+    width: 100,
+    margin: 20,
+  },
+});
+
+export class ModalDemo extends React.Component<any, any> {
+  state = {
+    visible: false,
+  };
+  open = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+  render() {
+    return (<View>
+      <TouchableHighlight
+        activeOpacity={0.5}
+        style={[styles.button]}
+        onPress={this.open}
+        underlayColor="#a9d9d4"
+      >
+        <Text>show popup</Text>
+      </TouchableHighlight>
+      <Dialog
+        visible={this.state.visible}
+        onClose={this.onClose}
+        animationType="fade"
+        wrapStyle={{
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        style={{
+          width: 300,
+        }}
+      >
+        <View style={{ padding: 100 }}>
+          <Text>
+            content
+          </Text>
+        </View>
+      </Dialog>
+    </View>);
+  }
+}
+
+export const Demo = ModalDemo;
+export const title = 'modal';
---
+++ typings/custom.d.ts
@@ -0,0 +1,4 @@
+declare module "rc-animate" {
+  var Ret: any;
+  export default Ret;
+}
