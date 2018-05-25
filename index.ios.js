f47e7fedd44c931bc805b139fcc40319ff9d5ce1
然则 <hualei.hl@alibaba-inc.com>
2018-05-25 14:38:33 +0800

    fix RN demo

--- index.ios.js
+++ index.ios.js
@@ -4,7 +4,7 @@ import getList from 'react-native-index-page';
 
 getList({
   demos: [
-    require('./examples/react-native/demo.native')
+    require('./_ts2js/examples/react-native/demo.native')
   ],
   title: require('./package.json').name,
 });
